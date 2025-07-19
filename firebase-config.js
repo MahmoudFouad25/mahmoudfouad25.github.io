f8aad5a88a77ea6c946f9bad7aa8c08db73cd321
MahmoudFouad25 <coach.mahmoud.fouad@gmail.com>
2025-07-19 04:23:32 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// سنحدث هذا الملف بمعلومات Firebase الحقيقية
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "fouad-coaching.firebaseapp.com",
+    projectId: "fouad-coaching",
+    storageBucket: "fouad-coaching.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase (سنضيف باقي الكود لاحقاً)
