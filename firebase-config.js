5cf6aa0a6393b5573aebb228312fd98ac3d42749
MahmoudFouad25 <coach.mahmoud.fouad@gmail.com>
2025-07-21 23:01:12 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCcJ_hNM4swSR1YC4dgGzoKIfYMTq3I71s",
-    authDomain: "fouad-coaching.firebaseapp.com",
-    projectId: "fouad-coaching",
-    storageBucket: "fouad-coaching.firebasestorage.app",
-    messagingSenderId: "182941771603",
-    appId: "1:182941771603:web:48deea9bc240f1b135e3a6",
-    measurementId: "G-80SPFZH1RS"
+  apiKey: "AIzaSyCcl_hWN4swSRlYC4dgGzoKIfYMTq3I71s",
+  authDomain: "fouad-coaching.firebaseapp.com",
+  projectId: "fouad-coaching",
+  storageBucket: "fouad-coaching.firebasestorage.app",
+  messagingSenderId: "182941771603",
+  appId: "1:182941771603:web:40deea9bc240f1b135e3a6",
+  measurementId: "G-00SPYZH1RS"
 };
 
 // Initialize Firebase
